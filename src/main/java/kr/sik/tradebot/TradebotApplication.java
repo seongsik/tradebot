f94964ba6b8656803787853a29bb54c79e052778
seongsik <soung371@gmail.com>
2024-02-13 10:47:36 +0900

    first commit

---
+++ src/main/java/kr/sik/tradebot/TradebotApplication.java
@@ -0,0 +1,13 @@
+package kr.sik.tradebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradebotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/sik/tradebot/TradebotApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.sik.tradebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
