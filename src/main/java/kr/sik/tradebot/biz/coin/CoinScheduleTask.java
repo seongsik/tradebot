4049ea048223a49c9d038a63ee08d1cbe23d1a64
seongsik <soung371@gmail.com>
2024-02-13 14:38:26 +0900

    scheduler 으로 변경

---
+++ src/main/java/kr/sik/tradebot/biz/coin/CoinScheduleTask.java
@@ -0,0 +1,21 @@
+package kr.sik.tradebot.biz.coin;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@EnableScheduling
+public class CoinScheduleTask {
+    private static final Logger logger = LoggerFactory.getLogger(CoinScheduleTask.class);
+
+    @Scheduled(fixedDelay = 5000)
+    public void run() {
+        logger.debug("CoinScheduleTask executed at: {}", new Date());
+    }
+}
